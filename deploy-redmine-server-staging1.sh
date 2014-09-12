f11b71b84d82efbe229d305562306ad4be3ea129
Sungwoo Park <swpark@nexell.co.kr>
2014-09-12 17:27:07 +0900

    [ADD] locale setting to redmine for postgresql

--- deploy-redmine-server-staging1.sh
+++ deploy-redmine-server-staging1.sh
@@ -31,3 +31,23 @@ gem install rdoc
 gem install rails -v 3.2.13
 echo "end"
 
+echo "setup locale UTF8 ---->"
+locale-gen en_US.UTF-8
+cat - > /etc/default/locale <<EOF
+LANG=en_US.UTF-8
+LANGUAGE=
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL=en_US.UTF-8
+EOF
+echo "end"
