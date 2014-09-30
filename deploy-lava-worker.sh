eb47a2a4c555ac7fb0b2b6cef89c24d11791643b
Sungwoo Park <swpark@nexell.co.kr>
2014-09-30 17:12:28 +0900

    [FIX] resolve lava-server -- lava-worker connection problem

--- deploy-lava-worker.sh
+++ deploy-lava-worker.sh
@@ -4,11 +4,16 @@
 # install target : ubuntu-12.04 precise #
 #########################################
 
+NEXELL_LAVA_SERVER=192.168.1.18
+INSTANCE_NAME=development
+
 echo "=================================================="
 echo "       deploy lava-worker for development"
 echo "=================================================="
 sudo apt-get update
 sudo apt-get install --yes git
+sudo apt-get install --yes android-tools-adb android-tools-fastboot
+sudo apt-get install --yes cu
 
 echo "download lava-deployment-tool ---->"
 cd $HOME
@@ -18,16 +23,16 @@ echo "end"
 echo "install lava-deployment-tool ---->"
 cd lava-deployment-tool
 ./lava-deployment-tool setup -nd
-SKIP_ROOT_CHECK=yes LAVA_DB_SERVER=192.168.1.231 LAVA_DB_NAME=lav-development LAVA_DB_USER=lava-development LAVA_DB_PASSWORD=rmsiddhk LAVA_REMOTE_FS_HOST=192.168.1.231 LAVA_REMOTE_FS_USER=nexell LAVA_REMOTE_FS_DIR=/srv/lava/instances/development LAVA_SERVER_IP=192.168.1.231 ./lava-deployment-tool installworker -nd lava-worker1
+SKIP_ROOT_CHECK=yes LAVA_DB_SERVER=${NEXELL_LAVA_SERVER} LAVA_DB_NAME=lav-${INSTANCE_NAME} LAVA_DB_USER=lava-${INSTANCE_NAME} LAVA_DB_PASSWORD=rmsiddhk LAVA_REMOTE_FS_HOST=${NEXELL_LAVA_SERVER} LAVA_REMOTE_FS_USER=nexell LAVA_REMOTE_FS_DIR=/srv/lava/instances/${INSTANCE_NAME} LAVA_MASTER=${NEXELL_LAVA_SERVER} ./lava-deployment-tool installworker -nd ${INSTANCE_NAME}
 echo "end"
 
 echo "set up development source ---->"
 cd $HOME
 git clone http://git.nexell.co.kr:8081/nexell/infrastructure/lava-server
 git clone http://git.nexell.co.kr:8081/nexell/infrastructure/lava-dispatcher
-/srv/lava/instances/lava-worker1/bin/lava-develop-local ~/lava-server
-/srv/lava/instances/lava-worker1/bin/lava-develop-local ~/lava-dispatcher
+/srv/lava/instances/${INSTANCE_NAME}/bin/lava-develop-local ~/lava-server
+/srv/lava/instances/${INSTANCE_NAME}/bin/lava-develop-local ~/lava-dispatcher
 
-sudo start lava-instance LAVA_INSTANCE=lava-worker1
+sudo start lava-instance LAVA_INSTANCE=${INSTANCE_NAME}
 
 echo "end"
