3150db7849bf5fca565b3ad5b7af3d46a2cb6d5a
Sungwoo Park <swpark@nexell.co.kr>
2014-10-06 20:31:14 +0900

    [FIXBUG] file-server

--- deploy-file-server.sh
+++ deploy-file-server.sh
@@ -31,7 +31,8 @@ sudo pip install south textile BeautifulSoup requests
 echo "end"
 
 echo "Setup install directory ---->"
-sudo chown -R $(whoami):$(whoami) /srv
+#sudo chown -R $(whoami):$(whoami) /srv
+sudo chown $(whoami):$(whoami) /srv
 mkdir -p /srv/${DIR}
 echo "end"
 
@@ -46,12 +47,12 @@ echo "end"
 echo "Setup DB ---->"
 export PYTHONPATH=/srv/${DIR}:/srv/${DIR}/linaro-license-protection:/srv/${DIR}/configs/django
 mkdir -p /srv/${DIR}/db
-django-admin syncdb --noinput
-django-admin collectstatic --noinput
+django-admin.py syncdb --noinput
+django-admin.py collectstatic --noinput
 echo "end"
 
 echo "Setup apache2 ---->"
-sudo cp -a /srv/${DIR}/configs/apache/${TYPE}.conf /etc/apache2/
+sudo cp -a /srv/${DIR}/configs/apache/${TYPE}.conf /etc/apache2/sites-available
 sudo a2dissite default
 sudo a2ensite ${TYPE}.conf
 echo "end"
