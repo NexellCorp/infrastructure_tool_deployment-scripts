a0aeb51c79300a45feafe829f6601d674240cd42
Sungwoo Park <swpark@nexell.co.kr>
2014-10-08 16:39:26 +0900

    [FIX] some permissions for file server

--- deploy-file-server.sh
+++ deploy-file-server.sh
@@ -34,6 +34,8 @@ echo "Setup install directory ---->"
 #sudo chown -R $(whoami):$(whoami) /srv
 sudo chown $(whoami):$(whoami) /srv
 mkdir -p /srv/${DIR}
+sudo chown www-data:www-data /srv/${DIR}
+sudo chmod 755 /srv/${DIR}
 echo "end"
 
 sudo apt-get install libapache2-mod-xsendfile
@@ -51,6 +53,8 @@ echo "Setup DB ---->"
 export PYTHONPATH=/srv/${DIR}:/srv/${DIR}/linaro-license-protection:/srv/${DIR}/configs/django
 export DJANGO_SETTINGS_MODULE="settings_snapshots"
 mkdir -p /srv/${DIR}/db
+sudo chown www-data:www-data /srv/${DIR}/db
+sudo chmod 755 /srv/${DIR}/db
 django-admin.py syncdb
 python /srv/${DIR}/linaro-license-protections/manage.py migrate license_protected_downloads 0001 --fake --settings="settings_snapshots" && touch /srv/${DIR}/db/.south-setup
 django-admin.py migrate
