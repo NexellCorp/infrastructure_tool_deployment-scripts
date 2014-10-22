fb409b429afa7d591a93d74cceab34a2f6fd7d04
Sungwoo Park <swpark@nexell.co.kr>
2014-10-22 16:11:26 +0900

    [ADD] deploy-jenkins

---
+++ deploy-jenkins.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+echo "Install jenkins"
+wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-get update
+sudo apt-get install --yes jenkins
+
+echo "Install plugins"
+sudo su jenkins
+cd
+cd plugins
+echo "===> scripttrigger"
+curl -O https://updates.jenkins-ci.org/latest/scripttrigger.hpi
+curl http://localhost/reload
+exit
+
+echo "Install End"
