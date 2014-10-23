e0f2c7b9d043c0e5984c8fdc14b367e2d6c98072
Sungwoo Park <swpark@nexell.co.kr>
2014-10-23 19:21:01 +0900

    [ADD] resolve some dependency for deploy jenkins

--- deploy-jenkins.sh
+++ deploy-jenkins.sh
@@ -5,6 +5,9 @@ wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add
 sudo sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
 sudo apt-get update
 sudo apt-get install --yes jenkins
+sudo apt-get install --yes libcurl4-openssl-dev
+sudo apt-get install --yes python-pip
+sudo apt-get install --yes python-pycurl
 
 echo "Install plugins"
 sudo su jenkins
